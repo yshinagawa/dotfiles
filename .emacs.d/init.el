a6a58e9cec92a769e279d7d773bd3095e82c6a85
yoshiki shinagawa <yosh.sn@gmail.com>
2014-05-07 20:58:41 +0900

    fix frame-title-format to display file path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -150,12 +150,15 @@
   (add-to-list 'default-frame-alist '(height . 48))
   (add-to-list 'default-frame-alist '(width . 108))
   (set-face-attribute 'default nil
-                     :family "Source Code Pro"
-                     :height 130)
+                      :family "Source Code Pro"
+                      :height 130)
   (set-fontset-font nil 'japanese-jisx0208
                     (font-spec :family "Hiragino Maru Gothic Pro"))
   (global-hl-line-mode t)
-  (setq frame-title-format (format "%%b @ Emacs %s" emacs-version))
+  (setq frame-title-format
+        '((:eval (if (buffer-file-name)
+                     (abbreviate-file-name (buffer-file-name))
+                   "%b")) " @ Emacs " emacs-version))
   (setq ring-bell-function 'ignore)
 
   ;; magit
