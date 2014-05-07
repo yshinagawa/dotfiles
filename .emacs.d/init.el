2e30bb335f31cf5818ace99000b284ed7582c6c4
yoshiki shinagawa <yosh.sn@gmail.com>
2014-05-08 02:10:14 +0900

    add ruby-lint configurations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -153,6 +153,17 @@
           (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
           line-end))
   :modes (enh-ruby-mode))
+(flycheck-define-checker ruby-rubylint
+  "A Ruby syntax and style checker using the rubylint tool."
+  :command ("ruby-lint" source)
+  :error-patterns
+  ((warning line-start
+            (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+            line-end)
+   (error line-start
+          (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+          line-end))
+  :modes (enh-ruby-mode))
 
 ;; general key-map
 (define-key global-map (kbd "C-m") 'newline-and-indent)
