8077e8d6b7487f4596552dd781d9f81f25c8d37f
yoshiki shinagawa <yosh.sn@gmail.com>
2014-05-26 11:58:05 +0900

    change key-bind for helm-mini in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,7 +79,7 @@
 (require 'helm-ag)
 (require 'helm-descbinds)
 (helm-descbinds-mode)
-(global-set-key (kbd "C-;") 'helm-mini)
+(global-set-key (kbd "C-c ;") 'helm-mini)
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "C-c b") 'helm-descbinds)
 (global-set-key (kbd "C-c o") 'helm-occur)
