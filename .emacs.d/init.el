1b8af73de86bb4c629262642fafc9bd0eb69d776
yoshiki shinagawa <yosh.sn@gmail.com>
2014-09-04 01:40:04 +0900

    add js2-mode-hook in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -160,7 +160,11 @@
 (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
 
 ;; js2-mode
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(setq js2-basic-offset 2)
+(setq js2-highlight-level 3)
+(add-hook 'js2-mode-hook (lambda () (electric-layout-mode -1)))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 ;; flycheck
 (require 'flycheck)
