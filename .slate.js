401ceb2ac6757127d8ade0f7194d0826fa53fc77
yshinagawa <yosh.sn@gmail.com>
2018-10-26 15:49:54 +0900

    Remove .slate.js .vimperatorrc mypy.lock.yaml

--- .slate.js
+++
@@ -1,9 +0,0 @@
-// Configs
-S.cfga({
-    "menuBarIconHidden": true,
-    "defaultToCurrentScreen": true,
-    "secondsBetweenRepeat": 0.1,
-    "checkDefaultsOnLoad": true,
-    "focusCheckWidthMax": 3000,
-    "orderScreensLeftToRight": true
-});
