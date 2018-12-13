9edef0e6317584471a16fea31139c9e32857dc86
yshinagawa <yosh.sn@gmail.com>
2018-12-13 17:41:15 +0900

    install.sh: Normalize URLs

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # Download dotfiles repository to home directory
 
 # Run with either command:
-# curl -fsSL https://bitbucket.org/yshinagawa/dotfiles/raw/3d5c07dff78ec17c0dfb4662338bbebd53b0cd33/install.sh | sh
-# wget -qO-  https://bitbucket.org/yshinagawa/dotfiles/raw/3d5c07dff78ec17c0dfb4662338bbebd53b0cd33/install.sh | sh
+# curl -fsSL https://bitbucket.org/yshinagawa/dotfiles/raw/master/install.sh | sh
+# wget -qO-  https://bitbucket.org/yshinagawa/dotfiles/raw/master/install.sh | sh
 
 if [ -d "$HOME/.dotfiles" ]; then
     printf "\\033[31mError:\\033[0m ~/.dotfiles already exists on your home directory.\\n" >&2
@@ -12,16 +12,16 @@ fi
 
 if ![ -x "$(command -v git)" ]; then
     if [ -x "$(command -v curl)" ];then
-        curl -fsSL https://bitbucket.org/yshinagawa/dotfiles/get/3d5c07dff78e.zip -o "$HOME/dotfiles.zip"
+        curl -fsSL https://bitbucket.org/yshinagawa/dotfiles/get/master.zip -o "$HOME/dotfiles.zip"
         mkdir "$HOME/.dotfiles" && tar xzf "$HOME/dotfiles.zip" -C "$HOME/.dotfiles" --strip-components 1
         rm "$HOME/dotfiles.zip"
     elif [ -x "$(command -v wget)" ]; then
-        wget -O "$HOME/dotfiles.zip" https://bitbucket.org/yshinagawa/dotfiles/get/3d5c07dff78e.zip
+        wget -O "$HOME/dotfiles.zip" https://bitbucket.org/yshinagawa/dotfiles/get/master.zip 
         mkdir "$HOME/.dotfiles" && tar xzf "$HOME/dotfiles.zip" -C "$HOME/.dotfiles" --strip-components 1
         rm "$HOME/dotfiles.zip"
     fi
 else
-    git clone --recursive https://yshinagawa@bitbucket.org/yshinagawa/dotfiles.git "$HOME/.dotfiles"
+    git clone --recursive https://bitbucket.org/yshinagawa/dotfiles.git "$HOME/.dotfiles"
 fi
 
 if [ -d "$HOME/.dotfiles" ]; then
