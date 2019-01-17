e174dce7e40c6ca63c4e6920104ceb1416f997b0
yshinagawa <yosh.sn@gmail.com>
2019-01-16 23:49:27 -0800

    Extract common install process

--- setup.sh
+++ setup.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
+set -e
 
-# install basic tools
-tools=("make" "stow" "nkf" "tmux")
+tools=(
+        make
+        stow
+        nkf
+        tmux
+        shellcheck
+        silversearcher-ag
+      )
 
-for i in "${tools[@]}"; do
-    if ! (type "$i" > /dev/null 2>&1); then
-        sudo apt-get -q -y install "$i"
-    fi
-done
+sudo apt-get -q -y "${tools[@]}"
 
-if [ -f /usr/share/bash-completion/completions/git ]; then
-    sudo ln -s /usr/share/bash-completion/completions/git /etc/bash_completion.d/git
-fi
-# install development tools
-sudo apt-get -q -y install shellcheck \
-                           silversearcher-ag
-
-[ -e "$HOME/.bashrc" ] && rm "$HOME/.bashrc"
-
-# symlink to home
-cd "$HOME/.dotfiles" && make link
-
-# install fzf
-"$HOME"/.bash/fzf/install --bin --no-update-rc > /dev/null 2>&1
-if ! [[ -x "$HOME"/.bash/fzf/bin/fzf ]]; then
-    printf "\\033[31mError:\\033[0m Install fzf failed.\\n" >&2
+"$HOME"/.dotfiles/.bash/fzf/install --bin --no-update-rc > /dev/null 2>&1
+if ! [[ -x "$HOME"/.dotfiles/.bash/fzf/bin/fzf ]]; then
+    printf "\\033[31merror!\\033[0m fzf installation failed.\\n" >&2
     exit 1
 fi
