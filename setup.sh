da605b3511e454048013e86170f17127034d492b
yshinagawa <yosh.sn@gmail.com>
2019-01-17 01:09:02 -0800

    Fix fzf installer path

--- setup.sh
+++ setup.sh
@@ -14,8 +14,8 @@ for i in "${tools[@]}"; do
     sudo apt-get -q -y install "$i"
 done
 
-"$HOME"/.dotfiles/.bash/fzf/install --bin --no-update-rc > /dev/null 2>&1
-if ! [[ -x "$HOME"/.dotfiles/.bash/fzf/bin/fzf ]]; then
+"$HOME"/.dotfiles/bash/.bash/fzf/install --bin --no-update-rc > /dev/null 2>&1
+if ! [[ -x "$HOME"/.dotfiles/bash/.bash/fzf/bin/fzf ]]; then
     printf "\\033[31merror!\\033[0m fzf installation failed.\\n" >&2
     exit 1
 fi
