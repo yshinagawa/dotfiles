c8650112f62a10b1021b1e40f5bcb8b8d6140c8b
yshinagawa <yosh.sn@gmail.com>
2019-01-20 22:11:29 +0900

    Move install git compeletion to setup.sh

--- setup.sh
+++ setup.sh
@@ -14,8 +14,14 @@ for i in "${tools[@]}"; do
     sudo apt-get -q -y install "$i"
 done
 
+# install fzf
 "$HOME"/.dotfiles/bash/.bash/fzf/install --bin --no-update-rc > /dev/null 2>&1
 if ! [[ -x "$HOME"/.dotfiles/bash/.bash/fzf/bin/fzf ]]; then
     printf "\\033[31merror!\\033[0m fzf installation failed.\\n" >&2
     exit 1
+fi
+
+# install git-completion
+if [ -f /usr/share/bash-completion/completions/git ]; then
+    sudo cp /usr/share/bash-completion/completions/git /etc/bash_completion.d/git
 fi
