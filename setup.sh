168ee9bebb465248e545a32caf22cd7fc0e6ac2d
yshinagawa <yosh.sn@gmail.com>
2019-01-27 02:28:49 +0900

    Add .travis.yml

--- setup.sh
+++ setup.sh
@@ -14,16 +14,15 @@ for i in "${tools[@]}"; do
 done
 
 # install fzf
-"$HOME"/.dotfiles/bash/.bash/fzf/install --bin --no-update-rc > /dev/null 2>&1
-if ! [[ -x "$HOME"/.dotfiles/bash/.bash/fzf/bin/fzf ]]; then
-    printf "\\033[31merror!\\033[0m fzf installation failed.\\n" >&2
-    exit 1
-fi
+"$HOME"/.dotfiles/bash/.bash/fzf/install --bin --no-update-rc
 
 # install git-completion
 if [ -f /usr/share/bash-completion/completions/git ]; then
     sudo cp /usr/share/bash-completion/completions/git /etc/bash_completion.d/git
 fi
+
+# timezone
+sudo timedatectl set-timezone Asia/Tokyo
 
 # symlink dotfiles to home directory
 source "$HOME/.dotfiles/link.sh"
